74e051fa3c44c6b3a89369a52534ba805bf2f503
ymummaneni <smummaneni@monarchtractor.com>
2025-10-13 03:20:54 +0530

    chore: trajectories & body visualize

--- src/simulator/scripts/gazebo_utils.py
+++ src/simulator/scripts/gazebo_utils.py
@@ -6,6 +6,8 @@ import numpy as np
 from std_srvs.srv import Empty
 from gazebo_msgs.srv import SetModelState
 from gazebo_msgs.msg import ModelState
+from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point
 
 
 def gazebo_reset_robot(start: tuple = (0.0, 0.0, 0.0)) -> None:
@@ -45,39 +47,111 @@ def gazebo_reset_robot(start: tuple = (0.0, 0.0, 0.0)) -> None:
         rospy.logerr(f"Failed to set robot state: {e}")
 
 
-def gazebo_visualize_path(
-    x: np.array, y: np.array, frame_id: str = "map", color: tuple = (0.0, 1.0, 0.0)
-) -> None:
+class RvizPathVisualizer:
     """
-    Visualizes a path in Gazebo using markers.
-    Args:
-        x (np.array): Array of x coordinates of the path.
-        y (np.array): Array of y coordinates of the path.
-        frame_id (str): The frame in which to visualize the path.
-        color (tuple): RGB color tuple for the path.
+    Class to visualize a path in RViz using markers.
     """
-    from visualization_msgs.msg import Marker
-    from geometry_msgs.msg import Point
 
-    marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size=10)
-    marker = Marker()
-    marker.header.frame_id = frame_id
-    marker.header.stamp = rospy.Time.now()
-    marker.ns = "path"
-    marker.id = 0
-    marker.type = Marker.LINE_STRIP
-    marker.action = Marker.ADD
-    marker.scale.x = 0.05  # Line width
-    marker.color.a = 1.0  # Alpha
-    marker.color.r = color[0]
-    marker.color.g = color[1]
-    marker.color.b = color[2]
+    def __init__(self, frame_id: str = "odom") -> None:
+        """
+        Initializes the RVizPathVisualizer.
+        Args:
+            frame_id (str): The frame in which to visualize the path.
+        """
+        self.global_path_pub = rospy.Publisher(
+            "/viz/path/global_path", Marker, latch=True
+        )
+        self.local_path_pub = rospy.Publisher(
+            "/viz/path/local_path", Marker, queue_size=10
+        )
+        self.robot_location_pub = rospy.Publisher(
+            "/viz/path/robot_location", Marker, queue_size=10
+        )
+        self.frame_id = frame_id
 
-    for xi, yi in zip(x, y):
-        point = Point()
-        point.x = xi
-        point.y = yi
-        point.z = 0.0
-        marker.points.append(point)
+    def visualize_local_path(self, x: np.array, y: np.array) -> None:
+        """
+        Visualizes a path in RViz using markers.
+        Args:
+            x (np.array): Array of x coordinates of the path.
+            y (np.array): Array of y coordinates of the path.
+        """
 
-    marker_pub.publish(marker)
+        marker = Marker()
+        marker.header.frame_id = self.frame_id
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = "path"
+        marker.id = 0
+        marker.type = Marker.LINE_STRIP
+        marker.action = Marker.MODIFY
+        marker.scale.x = 0.05
+        marker.color.a = 1.0
+        marker.color.r = 1.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.pose.orientation.w = 1.0
+        marker.points = []
+        for xi, yi in zip(x, y):
+            point = Point()
+            point.x = xi
+            point.y = yi
+            point.z = 0.0
+            marker.points.append(point)
+        self.local_path_pub.publish(marker)
+
+    def visualize_global_path(self, x: np.array, y: np.array) -> None:
+        """
+        Visualizes a path in RViz using markers.
+        Args:
+            x (np.array): Array of x coordinates of the path.
+            y (np.array): Array of y coordinates of the path.
+        """
+
+        marker = Marker()
+        marker.header.frame_id = self.frame_id
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = "path"
+        marker.id = 0
+        marker.type = Marker.LINE_STRIP
+        marker.action = Marker.ADD
+        marker.scale.x = 0.05
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 1.0
+        marker.color.b = 0.0
+        marker.pose.orientation.w = 1.0
+        marker.points = []
+        for xi, yi in zip(x, y):
+            point = Point()
+            point.x = xi
+            point.y = yi
+            point.z = 0.0
+            marker.points.append(point)
+        self.global_path_pub.publish(marker)
+
+    def visualize_robot_location(self, x: float, y: float) -> None:
+        """
+        Visualizes the robot location in RViz using a Point marker.
+        Args:
+            x (float): x coordinate of the robot.
+            y (float): y coordinate of the robot.
+        """
+        marker = Marker()
+        marker.header.frame_id = self.frame_id
+        marker.header.stamp = rospy.Time.now()
+        marker.ns = "robot"
+        marker.id = 0
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
+        marker.scale.x = 0.2
+        marker.scale.y = 0.2
+        marker.scale.z = 0.2
+        marker.color.a = 1.0
+        marker.color.r = 0.0
+        marker.color.g = 0.0
+        marker.color.b = 1.0
+        marker.pose.orientation.w = 1.0
+        marker.pose.position.x = x
+        marker.pose.position.y = y
+        marker.pose.position.z = 0.1
+        self.robot_location_pub.publish(marker)
