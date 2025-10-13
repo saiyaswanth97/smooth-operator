48e27eee3587009e652be0e73cac991d534c22cb
ymummaneni <smummaneni@monarchtractor.com>
2025-10-13 18:25:23 +0530

    add: basic controllers, remove mpc

--- src/mpc_controller/src/mpc_node.cpp
+++ src/mpc_controller/src/mpc_node.cpp
@@ -7,46 +7,140 @@
 #include <simulator/Path2D.h>
 #include <std_msgs/Float64.h>
 
-const struct DummyControllerConfig {
-    const double k_p_yaw;
-    const double velocity;
-} config = {
-  2.5,  // k_p_yaw
-  0.5   // velocity
-};
+#include <Eigen/Dense>
 
-// NOLINTNEXTLINE(google-runtime-references)
-bool mpc_service_callback(simulator::MPCService::Request &req,
-                          // NOLINTNEXTLINE(google-runtime-references)
-                          simulator::MPCService::Response &res) {
-    // double qx = req.odom.pose.pose.orientation.x;
-    // double qy = req.odom.pose.pose.orientation.y;
-    double qz = req.odom.pose.pose.orientation.z;
-    double qw = req.odom.pose.pose.orientation.w;
-    // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-    double yaw = atan2(2.0 * (qw * qz), 1.0 - 2.0 * (qz * qz));
-    double target_yaw = req.path.points[0].theta;
-    // double velocity = req.odom.twist.twist.linear.x;
+#include "OsqpEigen/OsqpEigen.h"
+// #include "mpc_solver_new1.h"
+
+// Constants for quaternion to yaw conversion
+constexpr double QUAT_TO_YAW_FACTOR = 2.0;
+
+// Default control parameters
+constexpr double DEFAULT_K_P_YAW_DUMB = 2.5;
+constexpr double DEFAULT_VELOCITY_DUMB = 0.5;
+constexpr double DEFAULT_K_P_YAW_STANLEY = 1.0;
+constexpr double DEFAULT_VELOCITY_STANLEY = 1.0;
+
+// Stanley controller lookahead index
+constexpr int STANLEY_LOOKAHEAD_INDEX = 10;
+
+std::tuple<double, double> dumb_control(simulator::Path2D path, const nav_msgs::Odometry &odom,
+                                        double k_p_yaw = DEFAULT_K_P_YAW_DUMB,
+                                        double velocity = DEFAULT_VELOCITY_DUMB) {
+    // Extract current yaw from odometry
+    double qz = odom.pose.pose.orientation.z;
+    double qw = odom.pose.pose.orientation.w;
+    double yaw = atan2(QUAT_TO_YAW_FACTOR * (qw * qz), 1.0 - QUAT_TO_YAW_FACTOR * (qz * qz));
+
+    // Target yaw from the first point in the path
+    if (path.points.empty()) {
+        return std::make_tuple(0.0, 0.0);
+    }
+    double target_yaw = path.points[0].theta;
+
+    // Compute yaw error
     double yaw_error = target_yaw - yaw;
-    // if (yaw_error > M_PI) {
-    //     yaw_error -= 2 * M_PI;
-    // } else if (yaw_error < -M_PI) {
-    //     yaw_error += 2 * M_PI;
-    // }
+    if (yaw_error > M_PI) {
+        yaw_error -= 2 * M_PI;
+    } else if (yaw_error < -M_PI) {
+        yaw_error += 2 * M_PI;
+    }
 
-    res.command.angular_velocity = yaw_error * config.k_p_yaw;
-    res.command.linear_velocity = config.velocity;
-    return true;
+    // Proportional control for angular velocity
+    double angular_velocity = k_p_yaw * yaw_error;
+
+    // Constant linear velocity
+    double linear_velocity = velocity;
+
+    return std::make_tuple(linear_velocity, angular_velocity);
 }
+
+std::tuple<double, double> stanley_control(simulator::Path2D path, const nav_msgs::Odometry &odom,
+                                           double k_p_yaw = DEFAULT_K_P_YAW_STANLEY,
+                                           double velocity = DEFAULT_VELOCITY_STANLEY) {
+    // Extract current yaw from odometry
+    double qz = odom.pose.pose.orientation.z;
+    double qw = odom.pose.pose.orientation.w;
+    double yaw = atan2(QUAT_TO_YAW_FACTOR * (qw * qz), 1.0 - QUAT_TO_YAW_FACTOR * (qz * qz));
+
+    // Target yaw from the first point in the path
+    if (path.points.empty()) {
+        return std::make_tuple(0.0, 0.0);
+    }
+
+    double x = odom.pose.pose.position.x;
+    double y = odom.pose.pose.position.y;
+    double target_x = path.points[STANLEY_LOOKAHEAD_INDEX].x;
+    double target_y = path.points[STANLEY_LOOKAHEAD_INDEX].y;
+    double target_yaw = std::atan2(target_y - y, target_x - x);
+
+    // Compute yaw error
+    double yaw_error = target_yaw - yaw;
+    if (yaw_error > M_PI) {
+        yaw_error -= 2 * M_PI;
+    } else if (yaw_error < -M_PI) {
+        yaw_error += 2 * M_PI;
+    }
+
+    // Proportional control for angular velocity
+    double angular_velocity = k_p_yaw * yaw_error;
+
+    // Constant linear velocity
+    double linear_velocity = velocity;
+
+    return std::make_tuple(linear_velocity, angular_velocity);
+}
+
+class MPCController {
+public:
+    // NOLINTNEXTLINE(google-runtime-references)
+    MPCController(ros::NodeHandle &nh, double k_p_yaw, double velocity)
+        : k_p_yaw_(k_p_yaw), velocity_(velocity) {
+        // : k_p_yaw_(k_p_yaw), velocity_(velocity), mpc_solver_(MPCSolver::Config()) {
+        service_ = nh.advertiseService("mpc_control", &MPCController::serviceCallback, this);
+    }
+
+    // NOLINTNEXTLINE(google-runtime-references, readability-make-member-function-const)
+    bool serviceCallback(simulator::MPCService::Request &req,
+                         // NOLINTNEXTLINE(google-runtime-references)
+                         simulator::MPCService::Response &res) {
+        // Solve MPC
+        // mpc_solver_.reset();
+        // simulator::Command command = mpc_solver_.solve(req.odom, req.path);
+
+        // Use stanley controller
+        // simulator::Command command;
+        // std::tie(command.linear_velocity, command.angular_velocity) =
+        //     stanley_control(req.path, req.odom, k_p_yaw_, velocity_);
+
+        // Use dumb controller
+        simulator::Command command;
+        std::tie(command.linear_velocity, command.angular_velocity) =
+          dumb_control(req.path, req.odom, k_p_yaw_, velocity_);
+
+        // Fill response
+        res.command = command;
+        std::cout << "Linear Velocity: " << command.linear_velocity
+                  << ", Angular Velocity: " << command.angular_velocity << std::endl;
+        return true;
+    }
+
+private:
+    double k_p_yaw_;
+    double velocity_;
+    // MPCSolver mpc_solver_;
+    ros::ServiceServer service_;
+};
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "mpc_node");
     ros::NodeHandle nh;
 
-    ros::ServiceServer service = nh.advertiseService("mpc_control", mpc_service_callback);
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay, cppcoreguidelines-pro-type-vararg)
-    ROS_INFO("MPC Controller Service Ready.");
+    // Initialize controller with gains
+    // NOLINTNEXTLINE(readability-magic-numbers,cppcoreguidelines-avoid-magic-numbers)
+    MPCController controller(nh, 2.5, 0.5);
 
+    // ROS_INFO("MPC Controller Service Ready.");
     ros::spin();
     return 0;
 }
